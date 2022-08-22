faf6eb67ba479885c2808a1c8fe9d3ed2eeabbec
abilov.w <109854878+Shirxan@users.noreply.github.com>
2022-08-22 16:57:13 +0300

    Add files via upload

---
+++ ev.t.7/script.js
@@ -0,0 +1,27 @@
+
+
+// function Fank(){
+//     let x = document.getElementById("one").innerHTML;
+// let y = document.getElementById("two").innerHTML;
+//     Swal.fire({
+//         position: 'top-end',
+//         icon: 'success',
+//         title: 'Your work has been saved',
+//         showConfirmButton: false,
+//         timer: 1500
+//       })
+//       console.log(x.value)
+
+// }
+alert("Salam sehfeye xow geldiniz")
+function Fank(){
+    let name = "admin@gmail.com";
+let pass = 12345678;
+let userName = document.getElementById("one").innerHTML;
+let userPass = document.getElementById("two").innerHTML;
+console.log(userName.value);
+console.log(userPass.value);
+if (userName.value == name && userPass.value == pass) {
+    alert("duzdu")
+}
+}
